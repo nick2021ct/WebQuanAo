a43167033399c82bd84dc95ea19f2445acc4eba7
nick2021ct <nick2021ct@gmail.com>
2024-12-03 01:47:51 +0700

    add cart

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    public function addToCart(Request $request, $idProduct)
+    {   
+        $request->validate([
+            'size' => 'required',
+            'qty' => 'required|integer|gt:0',
+        ],[
+            'size.required' => 'Vui lòng chọn size.',
+            'qty.required' => 'Vui lòng chọn số lượng.',
+            'qty.gt' => 'Số lượng phải lớn hơn 1.'
+        ]);
+
+        $product = Product::with('size')->findOrFail($idProduct);
+
+        $carts = Cart::where('idUser', Auth::user()->id)
+        ->where('idOrder', null)
+        ->get();
+
+        $check = true;
+
+        if (!isset($product->size->{$request->size}) || $product->size->{$request->size} < $request->qty) {
+            toastr()->error('Sản phẩm nhiều hơn kho hàng','Error');
+            return redirect()->back();
+        }
+
+        foreach ($carts as $cart) {
+        //TH sản phẩm trong giỏ cùng loại với sản phẩm vừa thêm
+        if ($cart->idProduct == $idProduct && $cart->size == $request->size) {
+            Cart::where('id', $cart->id)->update(['qty' => $cart->qty + $request->qty]);
+            $check = false;
+            break;
+        }
+        }
+        if ($check == true) {
+            Cart::insert([
+                'idProduct' => $idProduct,
+                'qty' => $request->qty,
+                'size' => $request->size,
+                'idUser' => Auth::user()->id,
+            ]);
+            toastr()->success('Thêm giỏ hàng thành công','Success');
+
+        }
+        return redirect()->back();
+    }
     public function viewCart()
     {
-        return view('order.cart');
+        $carts = Cart::where('idUser', Auth::user()->id)
+        ->where('idOrder', null)
+        ->with('product')
+        ->get();
+        $totalBill = 0;
+        foreach ($carts as $cart) {
+            $cart->total = $cart->product->priceSale * $cart->qty;
+            $totalBill += $cart->total;
+        }
+        return view('order.cart', compact('carts', 'totalBill'));
     }
 
     public function getFormCheckOut()
--- resources/views/order/cart.blade.php
+++ resources/views/order/cart.blade.php
@@ -12,7 +12,7 @@
                         <h4>Giỏ hàng</h4>
                         <div class="breadcrumb__links">
                             <a href="/">Trang chủ</a>
-                            <a href="">Sản phẩm</a>
+                            <a href="{{ route('listProduct') }}">Sản phẩm</a>
                             <span>Giỏ hàng</span>
                         </div>
                     </div>
@@ -25,6 +25,7 @@
     <!-- Giỏ hàng Section Begin -->
     <section class="shopping-cart spad">
         <div class="container">
+            @if (isset($carts[0]->id))
                 <div class="row">
                     <div class="col-lg-8">
                         <div class="shopping__cart__table">
@@ -39,42 +40,45 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <form action="" method="post">
-                                        
+                                    <form action="{{ route('updateCart') }}" method="post">
+                                        @csrf
+                                        @method('POST')
+                                        @foreach ($carts as $cart)
                                             <tr>
                                                 <td class="product__cart__item">
                                                     <div class="product__cart__item__pic">
-                                                        <img src="{{ asset('images/products/') }}"
+                                                        <img src="{{ asset('storage/images/products/' . $cart->product->images->first()->srcImage) }}"
                                                             alt="" width="80">
                                                     </div>
                                                     <div class="product__cart__item__text">
-                                                        <h6></h6>
-                                                        <h5 class="format-currency"></h5>
+                                                        <h6>{{ $cart->product->name }}</h6>
+                                                        <h5 class="format-currency">{{ $cart->product->priceSale }}đ</h5>
                                                         <h5><del style="font-size: 15px"
-                                                                class="text-danger format-currency"></del>
+                                                                class="text-danger format-currency">{{ $cart->product->price }}đ</del>
                                                         </h5>
                                                     </div>
                                                 </td>
                                                 <td class="quantity__item">
                                                     <div class="quantity">
                                                         <div class="pro-qty-2">
-                                                            <input type="text" value=""
-                                                                name="" min="1">
+                                                            <input type="text" value="{{ $cart->qty }}"
+                                                                name="{{ $cart->id }}" min="1">
                                                         </div>
                                                     </div>
                                                 </td>
-                                                <td class="cart__price"></td>
-                                                <td class="cart__price  format-currency">đ</td>
-                                                <td class="cart__close"><a href=""><i
+                                                <td class="cart__price">{{ $cart->size }}</td>
+                                                <td class="cart__price  format-currency">{{ $cart->total }}đ</td>
+                                                <td class="cart__close"><a href="{{ route('deleteInCart', $cart->id) }}"><i
                                                             class="fa fa-close"></i></a></td>
                                             </tr>
+                                        @endforeach
                                 </tbody>
                             </table>
                         </div>
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6">
                                 <div class="continue__btn">
-                                    <a href="">Tiếp tục mua sắm</a>
+                                    <a href="{{ route('listProduct') }}">Tiếp tục mua sắm</a>
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6">
@@ -92,13 +96,22 @@
                             <ul>
                                 <li>Phí vận chuyển <span>Miễn phí</span></li>
                                 <li>Tổng giá trị <span>
-                                </li>
+                                        @if (isset($voucher))
+                                            <span class="format-currency">{{ $totalBill - $voucher->value }}đ</span> <del
+                                                style="font-size: 15px; color:black;"
+                                                class="format-currency">{{ $totalBill }}đ</del>
+                                        @else
+                                            <span class="format-currency">{{ $totalBill }}đ</span>
+                                        @endif
+                                    </span></li>
                             </ul>
-                            <a href="" class="primary-btn">Thanh toán</a>
+                            <a href="{{ route('checkOut') }}" class="primary-btn">Thanh toán</a>
                         </div>
                     </div>
                 </div>
+            @else
                 <h3>Bạn chưa có sản phẩm nào trong giỏ. Hãy mua sắm!</h3>
+            @endif
         </div>
     </section>
     <!-- Giỏ hàng Section End -->
