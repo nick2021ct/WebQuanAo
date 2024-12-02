ec2aa4b6bac3295f8b40c7434f95306b9d5e95ed
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-03 00:20:37 +0700

    hoan thien chuc nang them banner

---
+++ app/Http/Controllers/BannerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Banner;
+use Illuminate\Http\Request;
+
+class BannerController extends Controller
+{
+    public function index()
+    {
+        $banners = Banner::all();
+        return view('admin.banner.index', compact('banners'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.banner.create');
+    }
+}
