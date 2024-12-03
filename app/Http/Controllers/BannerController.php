71a5da6cec4fa3dfe7361bcd78e24fbed03728eb
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-04 00:03:34 +0700

    fix them banner va lam chuc nang sua banner

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -20,4 +20,46 @@ class BannerController extends Controller
     {
         return view('admin.banner.create');
     }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:banners',
+            'image' => 'required',
+        ]);
+        $image = $request->image->getClientOriginalName();
+        $request->image->storeAs('public/images/banners', $image);
+        Banner::create([
+            'name' => $request->name,
+            'srcImage' => $image,
+        ]);
+        toastr()->success('Successfully', 'Added banner');
+        return redirect()->route('banner.index');
+    }
+    public function edit(Banner $banner)
+    {
+        return view('admin.banner.edit', compact('banner'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Banner $banner)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        if($request->image != null){
+            $image = $request->image->getClientOriginalName();
+            $request->image->storeAs('public/images/banners', $image);
+        }else{
+            $image = $banner->srcImage;
+        }
+        $banner->fill([
+            'name' => $request->name,
+            'srcImage' => $image,
+        ])->save();
+        toastr()->success('Successfully', 'Updated banner');
+        return redirect()->route('banner.index');
+    }
+
 }
