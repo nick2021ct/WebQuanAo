d38799ecb3370db40f9619b5c05524d341b09b47
phuckaka273 <phucnqph32664@fpt.edu.vn>
2024-12-05 22:17:01 +0700

    fixLocSanPham

--- app/Http/Controllers/AccountControllerAdmin.php
+++ app/Http/Controllers/AccountControllerAdmin.php
@@ -88,5 +88,118 @@ class AccountControllerAdmin extends Controller
         toastr()->success('Successfully', 'Created account');
         return redirect()->route('account.index');
     }
-
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('admin.account.edit', compact('user'));
+    }
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            // 'password' => 'required',
+            // 'repeat_password' => 'required|same:password',
+            'fullname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|max:10|min:10',
+            'address' => 'required',
+        ]);
+        $data = [
+            'username' => $request->username,
+            // 'password' => Hash::make($request->password),
+            'fullname' => $request->fullname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            // 'role' => $request->role,
+        ];
+        User::where('id', $id)->update($data);
+        $user = User::findOrFail($id);
+        //set lại quyền cho tk nhân viên
+        $roles = ['Account', 'Category', 'Brand', 'Product', 'Banner', 'Voucher', 'Blog'];
+        $actions = ['show', 'add', 'update', 'delete'];
+        $permission = [];
+        $revokePermission = [];
+        foreach ($roles as $role) {
+            foreach ($actions as $action) {
+                array_push($revokePermission, $action . $role);
+                if ($request->input($action . $role) == 'on') {
+                    array_push($permission, $action . $role);
+                }
+            }
+        }
+        $user->revokePermissionTo($revokePermission);
+        $user->givePermissionTo($permission);
+        toastr()->success('Successfully', 'Updated account');
+        return redirect()->route('account.index');
+    }
+    public function destroy($id)
+    {
+        //Không thể tự block chính mình
+        if (Auth::user()->id == $id) {
+            toastr()->error('Unachievable', "You can't block yourself");
+            return redirect()->route('account.index');
+        }
+        User::where('id', $id)->delete();
+        toastr()->success('Successfully', 'Blocked account');
+        return redirect()->route('account.index');
+    }
+    public function unblock($id)
+    {
+        User::where('id', $id)->restore();
+        toastr()->success('Successfully', 'Unblocked account');
+        return redirect()->route('account.index');
+    }
+    public function getFormCreateStaff()
+    {
+        return view('admin.account.createStaff');
+    }
+    
+    
+    
+    public function submitFormCreateStaff(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|unique:users',
+            'password' => 'required',
+            'repeat_password' => 'required|same:password',
+            'fullname' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required|unique:users|max:10|min:10',
+            'address' => 'required',
+        ]);
+        $data = [
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+            'fullname' => $request->fullname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'role' => 2,
+        ];
+        $user = User::create($data);
+        // dd($user);
+        $token = Str::random(32);
+        User::where('email', $request->email)->update(['token' => $token]);
+        $information = [
+            'name' => $request->fullname,
+            'email' => $request->email,
+            'token' => $token
+        ];
+        Mail::to($request->email)->send(new VerifiEmail($information));
+        //set quyền cho tk nhân viên
+        $roles = ['Account', 'Category', 'Brand', 'Product', 'Banner', 'Voucher'];
+        $actions = ['show', 'add', 'update', 'delete'];
+        $permission = [];
+        foreach ($roles as $role) {
+            foreach ($actions as $action) {
+                if ($request->input($action . $role) == 'on') {
+                    array_push($permission, $action . $role);
+                }
+            }
+        }
+        $user->givePermissionTo($permission);
+        toastr()->success('Successfully', 'Created account');
+        return redirect()->route('admin.account.createStaff');
+    }
 }
