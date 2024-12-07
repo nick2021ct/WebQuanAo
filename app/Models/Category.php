1f69af4b4e98ded9f495e84214689805ead4bde8
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-07 23:54:22 +0700

    fix bug

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,13 +6,4 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
-{
-    use HasFactory;
-    use SoftDeletes;
-    protected $fillable = [
-        'name',
-        'description',
-    ];
-
-}
+class Category extend
