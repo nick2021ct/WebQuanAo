e2a2172a437aa9421d416348eb1c9b2ca71f698d
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-10 00:25:02 +0700

    fix bug

--- resources/views/admin/banner/edit.blade.php
+++ resources/views/admin/banner/edit.blade.php
@@ -70,42 +70,6 @@
                                             <td class="border-bottom-0" style="display: flex;">
                                              
                                             <  @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="container-fluid"
-                            @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="container-fluid"
-                            @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="container-fluid"
-                            @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="container-fluid"
-                            @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
+                          
+                            
                         
