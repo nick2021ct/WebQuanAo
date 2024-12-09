62d301deec8ed1d07b9568dd0cf1500935e5fac6
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-10 00:20:01 +0700

    fix edit banner

--- resources/views/admin/banner/edit.blade.php
+++ resources/views/admin/banner/edit.blade.php
@@ -55,18 +55,32 @@
                             </div>
                             <div class="container-fluid">
     <div class="container-fluid">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title fw-semibold mb-4">Thêm Banner</h5>
-                <div class="card">
-                    <div class="card-body">
-                        <form action="{{ route('banner.store') }}" method="post" enctype="multipart/form-data">
-                            @csrf
-                            @method('POST')
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
+    </td>
+                                            <td class="border-bottom-0">
+                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
+                                                    width="60" alt="">
+                                            </td>
+                                            <td class="border-bottom-0" style="display: flex;">
+                                             
+                                            </td>  </td>
+                                            <td class="border-bottom-0">
+                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
+                                                    width="60" alt="">
+                                            </td>
+                                            <td class="border-bottom-0" style="display: flex;">
+                                             
+                                            </td>  </td>
+                                            <td class="border-bottom-0">
+                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
+                                                    width="60" alt="">
+                                            </td>
+                                            <td class="border-bottom-0" style="display: flex;">
+                                             
+                                            </td>  </td>
+                                            <td class="border-bottom-0">
+                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
+                                                    width="60" alt="">
+                                            </td>
+                                            <td class="border-bottom-0" style="display: flex;">
+                                             
+                                            </td>
