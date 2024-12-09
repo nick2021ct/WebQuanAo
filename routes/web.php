0391a440265eb935cf0fee7218e1bebf139c8384
trantuan04 <trantuankoy762@gmail.com>
2024-12-10 00:05:07 +0700

    crudacount

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,19 @@ Route::group(['prefix' => 'admin', 'middleware' => 'AdminLogin'], function () {
     Route::get('/product', [ProductController::class, 'index'])->middleware('can:showProduct')->name('product.index');
     Route::get('/product/create', [ProductController::class, 'create'])->middleware('can:addProduct')->name('product.create');
     Route::post('/product', [ProductController::class, 'store'])->middleware('can:addProduct')->name('product.store');
+
+    Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->middleware('can:updateProduct')->name('product.edit');
+    Route::put('/product/{product}', [ProductController::class, 'update'])->middleware('can:updateProduct')->name('product.update');
+    Route::delete('/product/{product}', [ProductController::class, 'destroy'])->middleware('can:deleteProduct')->name('product.destroy');
+    Route::get('/product-restore/{id}', [ProductController::class, 'restore'])->middleware('can:deleteProduct')->name('admin.product.restore');
+
+    Route::get('/account', [AccountControllerAdmin::class, 'index'])->middleware('can:showAccount')->name('account.index');
+    Route::get('/account/create', [AccountControllerAdmin::class, 'create'])->middleware('can:addAccount')->name('account.create');
+    Route::post('/account', [AccountControllerAdmin::class, 'store'])->middleware('can:addAccount')->name('account.store');
+    Route::get('/account/{account}/edit', [AccountControllerAdmin::class, 'edit'])->middleware('can:updateAccount')->name('account.edit');
+    Route::put('/account/{account}', [AccountControllerAdmin::class, 'update'])->middleware('can:updateAccount')->name('account.update');
+    Route::delete('/account/{account}', [AccountControllerAdmin::class, 'destroy'])->middleware('can:deleteAccount')->name('account.destroy');
+    Route::get('/create-staff', [AccountControllerAdmin::class, 'getFormCreateStaff'])->name('admin.account.createStaff');
+    Route::post('/create-staff', [AccountControllerAdmin::class, 'submitFormCreateStaff'])->name('admin.account.createStaff');
+    Route::get('/account-unblock/{id}', [AccountControllerAdmin::class, 'unblock'])->middleware('can:deleteAccount')->name('admin.account.unblock');
 })
