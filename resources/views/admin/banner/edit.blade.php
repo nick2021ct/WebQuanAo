3c0fb50f0ba559c22d3df5e6266fdce5a2157cf7
trangntph41101 <trangnt41101@fpt.edu.vn>
2024-12-10 00:25:19 +0700

    fix bug

--- resources/views/admin/banner/edit.blade.php
+++ resources/views/admin/banner/edit.blade.php
@@ -48,28 +48,7 @@
                             @method('POST')
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Tên Banner</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                @error('name')
-                                <div id="emailHelp" class="text-danger">{{ $message }}</div>
-                                @enderror
-                            </div>
-                            <div class="container-fluid">
-    <div class="container-fluid">
-    </td>
-                                            <td class="border-bottom-0">
-                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
-                                                    width="60" alt="">
-                                            </td>
-                                            <td class="border-bottom-0" style="display: flex;">
-                                             
-                                            </td>  </td>
-                                            <td class="border-bottom-0">
-                                                <img src="{{ asset('storage/images/banners/' . $banner->srcImage) }}"
-                                                    width="60" alt="">
-                                            </td>
-                                            <td class="border-bottom-0" style="display: flex;">
-                                             
-                                            <  @method('POST')
-                          
+              
+                                            
                             
                         
