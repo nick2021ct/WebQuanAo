bba37bbcc655fa971b8fb929d766448027be2ebe
phuckaka273 <phucnqph32664@fpt.edu.vn>
2024-12-14 21:45:26 +0700

    fixCreateProduct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -59,7 +59,6 @@ class ProductController extends Controller
         $categories = Category::all();
         return view('admin.product.create', compact('brands', 'categories'));
     }
-
     /**
      * Store a newly created resource in storage.
      */
